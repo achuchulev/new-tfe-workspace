3a2cb9e9f67e4cabe35a4cf8c05ec95637496932
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-15 14:32:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ variable "token" {}
 variable "oauth" {}
 variable "organization" {}
 variable "CO_team" {}
-
-module "aws-navCore-workspace-test" {
-  source            = "app.terraform.io/NavicoCloudTeam/workspace-aws/tfe"
-  version           = "0.1.5"
+ 
+module "aws-workspace-test" {
+  source            = "app.terraform.io/atanasc/addworkspace/tfe"
+  version           = "0.1.1"
   workspace_type    = "repo"
-  workspace_name    = "navCore-workspace-test"
-  repo_working_dir  = "use1/inf/dev/networking"
+  workspace_name    = "workspace-test"
+  repo_working_dir  = ""
   tfe_team          = "${var.CO_team}"
   tfe_team_access   = "admin"
   token             = "${var.token}"
   oauth             = "${var.oauth}"
   tfe_organization  = "${var.organization}"
-  aws_account       = "navCore"
-  vcs_repo_ident    = "CO/navcore"
+  #aws_account       = "navCore"
+  vcs_repo_ident    = "achuchulev/tfe-aws-workspace"
 }
-module "var-navCore-workspace-test" {
-  source = "./aws/navCore-workspace-test"
-  region = "us-east-1"
-  account = "717547922293"
-  workspace_id = "${module.aws-navCore-workspace-test.tfe_repo_workspace_id}"
+module "var-workspace-test" {
+  source = "./aws/workspace-test"
+  region = "us-east-2"
+  account = "618541241448"
+  workspace_id = "${module.aws-workspace-test.tfe_repo_workspace_id}"
 }
