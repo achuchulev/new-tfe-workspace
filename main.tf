cc9cb597522fa3f8e248731f6569175ad97153f0
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-02-15 16:15:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ variable "CO_team" {}
  
 module "aws-workspace-test" {
   source            = "app.terraform.io/atanasc/addworkspace/tfe"
-  version           = "0.0.1"
+  version           = "0.0.9"
   workspace_type    = "repo"
   workspace_name    = "workspace-test"
   repo_working_dir  = ""
